9a48cbd41ddc31392024744d04631ff9eb93dbc3
Earvin Gemenez <earvin.gemenez@gmail.com>
2018-02-21 11:49:17 +0800

    added wowjs

--- assets/scripts/core/src/app/app.component.ts
+++ assets/scripts/core/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { WOW } from 'wowjs/dist/wow.min';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit() { new WOW().init(); }
+
 }
--- assets/scripts/core/src/app/public/public.module.ts
+++ assets/scripts/core/src/app/public/public.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { IndexComponent } from './index/index.component';
 
 @NgModule({
